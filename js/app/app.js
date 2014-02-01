a5bdd8eb7c9d75be5295d98f0e0b27a4aee03670
Tobias Foord <toby@idodev.co.uk>
2014-02-01 18:32:19 +0000

    Added newsletter signup

--- js/app/app.js
+++ js/app/app.js
@@ -94,7 +94,7 @@ $(function () {
     //track user clicking search result
     if (typeof (ga) === "function") {
         $('.site-search .results li a').on('click', function () {
-            ga('send', 'event', 'Search', 'Clicked Result', $(this).attr('href'));
+            ga('send', 'event', 'Website', 'Used Site Search', $(this).attr('href'));
         });
     }
 
@@ -159,3 +159,41 @@ $(function () {
 
 })(jQuery);
 
+
+/** http://stackoverflow.com/a/15120409/477958 **/
+$(function () {
+  var $form = $('#mc-embedded-subscribe-form');
+
+  $('#mc-embedded-subscribe').on('click', function(event) {
+    if(event) event.preventDefault();
+    register($form);
+  });
+});
+
+function register($form) {
+  $.ajax({
+    type: $form.attr('method'),
+    url: $form.attr('action'),
+    data: $form.serialize(),
+    cache       : false,
+    dataType    : 'json',
+    contentType: "application/json; charset=utf-8",
+    error       : function(err) { $('#notification_container').html('<span class="alert">Could not connect to server. Please try again later.</span>'); },
+    success     : function(data) {
+
+      if (data.result != "success") {
+        var message = data.msg.substring(4);
+        $('#notification_container').html('<span class="alert">'+message+'</span>');
+      }
+
+      else {
+        var message = data.msg;
+        $('#notification_container').html('<span class="success">'+message+'</span>');
+        //track user clicking search result
+        if (typeof (ga) === "function") {
+            ga('send', 'event', 'Website', 'Newsletter Sign Up');
+        }
+      }
+    }
+  });
+}
